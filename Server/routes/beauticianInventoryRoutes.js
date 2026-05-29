f70c5fdec5c5f940e3946e68bd111bb2e5fcf5d6
Yadu-krishnankv <yadhumanoj12@gmail.com>
2026-05-29 09:43:53 +0530

    Fix-route middleware

--- Server/routes/beauticianInventoryRoutes.js
+++ Server/routes/beauticianInventoryRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const authMiddleware = require('../middleware/authMiddleware');
+const { protect } = require('../middleware/authMiddleware');
 const beauticianInventoryController = require('../controllers/beauticianInventoryController');
 
 // Beautician inventory APIs
-router.get('/inventory', authMiddleware, beauticianInventoryController.listInventory);
-router.post('/inventory/use', authMiddleware, beauticianInventoryController.useInventoryItem);
-router.get('/inventory/history', authMiddleware, beauticianInventoryController.usageHistory);
 
+router.get('/inventory', protect, beauticianInventoryController.listInventory);
+router.post('/inventory/use', protect, beauticianInventoryController.useInventoryItem);
+router.get('/inventory/history', protect, beauticianInventoryController.usageHistory);
 // Admin inventory filter
-router.get('/admin/inventory', authMiddleware, beauticianInventoryController.adminFilterInventory);
+router.get('/admin/inventory', protect, beauticianInventoryController.adminFilterInventory);
 
 module.exports = router;
