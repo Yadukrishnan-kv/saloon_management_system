b7754324defe28a7f2b8c1ae5d6199b103cd5c81
Yadu-krishnankv <yadhumanoj12@gmail.com>
2026-05-30 14:36:29 +0530

    fix-qr code duplicate

--- Server/models/BeauticianInventory.js
+++ Server/models/BeauticianInventory.js
@@ -48,5 +48,7 @@ const BeauticianInventorySchema = new mongoose.Schema({
 }, { timestamps: true });
 
 BeauticianInventorySchema.index({ beauticianId: 1, productId: 1, status: 1 });
+// Prevent duplicate inventory per order/product/serial (qrCode is unique per quantity)
+BeauticianInventorySchema.index({ orderId: 1, productId: 1, qrCode: 1 }, { unique: true });
 
 module.exports = mongoose.model('BeauticianInventory', BeauticianInventorySchema);
